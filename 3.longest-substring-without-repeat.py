159f62a50aa2e32431f83d337eba9a86f0adee79
tNita <tgnt03891894@gmail.com>
2025-09-06 11:09:43 +0900

    3. コメント追加

--- 3.longest-substring-without-repeat.py
+++ 3.longest-substring-without-repeat.py
@@ -3,6 +3,8 @@ class Solution:
         subStr = set()
         maxLength = 0
         left = 0
+        # 1. 右端(right)が基準となる最大長の部分文字列を作る→前のループで作った最大長の部分文字列に右端を含めれるまで左端を切り詰める
+        # 2. 長さを求めて最大値を必要とあれば更新
         for right, c in enumerate(s):
             maxLength = max(len(subStr), maxLength)
             while c in subStr:
