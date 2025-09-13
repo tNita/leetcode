fe6f26923f96c8fffbf5da31aa3db2ecc85ecc3a
tNita <tgnt03891894@gmail.com>
2025-09-13 13:50:42 +0900

    kth symbol in grammer　他のアルゴリズム

--- python/779.kth-symbol-in-grammer.py
+++ python/779.kth-symbol-in-grammer.py
@@ -1,3 +1,14 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
-        return (k - 1).bit_count() & 1
+        # n行目のk番目へはk-1の2進数表現で頭から0（左）、1（右）とした道順
+        # 右に行けば反転するのでk-1の二進数表現の1の数の偶奇による
+        # return (k - 1).bit_count() & 1
+
+        # 再帰
+        # kが奇数→左子、,kが偶数→右子
+        # 親は k +1  // 2
+        if n == 1:
+            return 0
+
+        parent = self.kthGrammar(n - 1, (k + 1) // 2)
+        return parent if k % 2 == 1 else 1 - parent
