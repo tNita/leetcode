72ddd7f4a65e67c15d8bd845ff373b85bccefe66
tNita <tgnt03891894@gmail.com>
2025-09-18 09:19:43 +0900

    300 最長増加部分列の長さを計算する

---
+++ python/300.longest-increasing-subsequence.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        length = len(nums)
+        dp = [1] * length
+        for i in range(1, length):
+            for j in range(0, i):
+                if nums[i] > nums[j] and dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
+        return max(dp)
+
+        # 二分探索で
+        # tails = []  # 長さkの増加部分列の末尾最小値を tails[k-1] に保持
+        # for n in nums:
+        #     i = bisect_left(tails, n)  # x を置ける最左位置
+        #     if i == len(tails):
+        #         tails.append(x)
+        #     else:
+        #         tails[i] = x
+        # return len(tails)
