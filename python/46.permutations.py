557ec36aa9f4189c4cb0cf92d9dc79c9807bc9fb
tNita <tgnt03891894@gmail.com>
2025-09-19 09:40:13 +0900

    46 permutations別解追加

--- python/46.permutations.py
+++ python/46.permutations.py
@@ -10,3 +10,25 @@ class Solution:
                 s.insert(i, nums[0])
                 ans.append(s)
         return ans
+
+        # バックトラッキング
+        # ans = []
+        # # nums[0:first) はすでに確定した部分列
+        # def backtrack(first: int) -> None:
+        #     if first == len(nums):
+        #         ans.append(nums[:])
+        #         return
+        #     for i in range(first, len(nums)):
+        #         nums[first], nums[i] = nums[i], nums[first]  # 候補を先頭側に
+        #         backtrack(first + 1)                         # 次の位置へ
+        #         nums[first], nums[i] = nums[i], nums[first]  # ロールバック
+        # backtrack(0)
+        # return ans
+
+        # iterator tool使う
+        # ans = []
+        # # リストから順列を生成
+        # for p in itertools.permutations(nums, len(nums)):
+        #     ans.append(p)
+
+        # return ans
