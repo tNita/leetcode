c5643b705091bb5c23e2acfc94966070787ac91c
tNita <tgnt03891894@gmail.com>
2025-09-26 00:00:57 +0900

    add another solution for 62. Unique Paths

--- python/62.unique-paths.py
+++ python/62.unique-paths.py
@@ -2,10 +2,29 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         if m == 1 or n == 1:
             return 1
+        # def factorial(x: int) -> int:
+        #     if x == 1: return 1
+        #     return x * factorial(x-1)
+        # return factorial(m+n-2) // (factorial(m-1) * factorial(n-1))
 
-        def factorial(x: int) -> int:
-            if x == 1:
-                return 1
-            return x * factorial(x - 1)
+        dp = [[0 for _ in range(n)] for _ in range(m)]
 
-        return factorial(m + n - 2) // (factorial(m - 1) * factorial(n - 1))
+        for i in range(m):
+            dp[i][n - 1] = 1
+        for j in range(n):
+            dp[m - 1][j] = 1
+
+        stack = [(0, 0)]
+        while stack:
+            d, r = stack.pop()
+            if dp[d][r + 1] == 0:
+                stack.append((d, r))
+                stack.append((d, r + 1))
+                continue
+            if dp[d + 1][r] == 0:
+                stack.append((d, r))
+                stack.append((d + 1, r))
+                continue
+
+            dp[d][r] = dp[d][r + 1] + dp[d + 1][r]
+        return dp[0][0]
